baaa34af240e1474736f0cf0285361b702b6427d
juanabernstein <jbernstein@udesa.edu.ar>
2025-11-09 20:30:16 -0300

    terminando perfil, me falta agregar estilos

--- src/screens/Perfil/Perfil.js
+++ src/screens/Perfil/Perfil.js
@@ -1,19 +1,52 @@
 import React, { Component } from "react"
 import { View, Text, StyleSheet, FlatList, Pressable} from "react-native"
 import { db, auth } from "../../firebase/config";
-// agregar lo de posteos, nombre de usuario y mail 
-// hacer css
+import Posts from "../../components/Posts/Posts";
+
+// FALTA APLICAR ESTILOS
 
 
 class Perfil extends Component {
     constructor(props) {
         super(props)
         this.state = {
-            
+            posts : '' ,
+            loading : true ,
+            usuario : {}
         }
     }
 
-  
+    componentDidMount() {
+        
+        db.collection("posts")
+           .where('owner' , '==' , auth.currentUser.email)
+            .onSnapshot((docs) => {
+                let postsArray = [];
+                docs.forEach((doc) => {
+                    const data = doc.data()
+                    postsArray.push({
+                        id: doc.id,
+                        owner: data.owner,
+                        texto: data.texto,
+                        createdAt: data.createdAt,
+                        comentarios: data.comentarios ,
+                        likes: data.likes ,
+                    });
+                });
+                this.setState({ posts: postsArray, loading: false });
+            });
+
+            db.collection("users")
+            .where('email' , '==' , auth.currentUser.email)
+             .onSnapshot((docs) => {
+                 docs.forEach((doc) => {
+                     const data = doc.data()
+                     console.log(data)
+                     
+                     this.setState({usuario : data})
+                 });
+             });     
+    }
 
     logout(){
         auth.signOut()
@@ -23,23 +56,31 @@ class Perfil extends Component {
     render() {
         return (
             <View >
+                <Text> PERFIL </Text>
+                <Text> {this.state.usuario.email }</Text>
+                <Text> {this.state.usuario.username }</Text>
                 
                 <Pressable onPress={() => this.logout()}> 
                     <Text> Logout </Text>
                 </Pressable>
+
+                {this.state.loading ? (
+                    <Text>Cargando posts...</Text>
+                ) : (
+                    <FlatList
+                        data={this.state.posts}
+                        keyExtractor={(item) => item.id}
+                        renderItem={({ item }) => <Posts data={item} home={false} />}
+                    />
+
+                   
+                )}
     
             </View>
         )
     }
 
 }
-
-
-
-
-        
-
-
 
 
 export default Perfil;
