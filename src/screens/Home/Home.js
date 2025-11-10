dac314e1f8a65dc81d2af3f41d759fcd675f746a
juanabernstein <jbernstein@udesa.edu.ar>
2025-11-09 23:16:00 -0300

    arrancando el home, falta el renderizado

--- src/screens/Home/Home.js
+++ src/screens/Home/Home.js
@@ -1,14 +1,40 @@
 import React, { Component } from "react"
 import { View, Text, StyleSheet} from "react-native"
+import { db, auth } from "../../firebase/config";
+import Posts from "../../components/Posts/Posts"
 
-
-// agregar lo de crear posts cuanod este hech
+// agregar estilos 
 
 class Home extends Component {
     constructor(props) {
         super(props)
-        this.state = {}
+        this.state = {
+            posts : [] , 
+            loading : true ,
+        }
     }
+
+    componentDidMount() {
+        db.collection("posts")
+            .orderBy("createdAt", "desc")
+            .onSnapshot((docs) => {
+                let postsArray = [];
+                docs.forEach((doc) => {
+                    const data = doc.data()
+                    postsArray.push({
+                        id: doc.id,
+                        owner: data.owner,
+                        texto: data.texto,
+                        createdAt: data.createdAt,
+                        comentarios: data.comentarios ,
+                        likes: data.likes ,
+                    });
+                });
+                this.setState({ posts: postsArray, loading: false });
+            });
+    }
+
+
     render() {
         return (
             <View style={styles.click}>
