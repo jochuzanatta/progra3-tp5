ed5d7870ff9e096e6087c6c31551842d4f16098e
delficamuglia <dcamuglia@udesa.edu.ar>
2025-11-09 21:03:27 -0300

    creando estructura de crear posteo

---
+++ src/screens/CrearPosteo/CrearPosteo.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react"
+import { Text } from "react-native"
+
+class CrearPosteo extends Component {
+    constructor(props) {
+        super(props)
+        this.state= {
+
+        }
+    }
+
+    render() {
+        return (
+            <Text> Crear posteo </Text>
+        )
+    }
+}
